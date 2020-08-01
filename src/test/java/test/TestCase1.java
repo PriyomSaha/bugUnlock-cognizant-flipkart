867f5e6f6140449c0fb3038b8c63c6d8a005809a
Priyom Saha <priyom1499@gmail.com>
2020-08-01 14:06:56 +0530

    updated

--- src/test/java/test/TestCase1.java
+++ src/test/java/test/TestCase1.java
@@ -14,6 +14,7 @@ import utility.DriverSetup;
 import utility.Locator;
 import utility.TakeScreenShot;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -97,18 +98,17 @@ public class TestCase1 extends DriverSetup{
             all_data[3] = resultPrices.get(i).getText();
             int j = 0;
             for (String data : all_data) {
-                System.out.println(data);
-                /*Cell cell = row.createCell(j++);
-                cell.setCellValue(data);*/
+                Cell cell = row.createCell(j++);
+                cell.setCellValue(data);
             }
         }
 
-       /* try (FileOutputStream outputStream = new FileOutputStream(output_path + "\\Output\\output.xlsx")) {
+        try (FileOutputStream outputStream = new FileOutputStream(output_path + "\\Output\\output.xlsx")) {
             workbook.write(outputStream);
             ts.snapShot("result");
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
         Thread.sleep(10000);
 
     }
